db795c6afec4039a8553210ef02f1667664ba7d8
LumineTraveller <caoyj2006723@outlook.com>
2026-08-30 04:08:09 +0800

    chore: fix mobile test lint env and ignore local download site

--- mobile/.eslintrc.js
+++ mobile/.eslintrc.js
@@ -1,4 +1,13 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  overrides: [
+    {
+      files: ['__tests__/**/*.js', '__tests__/**/*.cjs'],
+      env: {
+        node: true,
+        jest: true,
+      },
+    },
+  ],
 };
